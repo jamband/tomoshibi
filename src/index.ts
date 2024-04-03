9167519531cdcf9c9f11479f66fbb09cf787b6cf
Tomoki Morita <tmsongbooks215@gmail.com>
2024-04-03 09:26:58 +0900

    use appendTrailingSlash middleware
    and remove unnecessary notFound method

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import type { Context } from "hono";
 import { Hono } from "hono";
+import { appendTrailingSlash } from "hono/trailing-slash";
 import { Layout } from "./layouts/layout";
 import Home from "./routes";
 import About from "./routes/about";
 import NotFound from "./routes/not-found";
 
-const app = new Hono();
-
-const notFoundHandler = (c: Context) =>
-  c.render(NotFound(), { title: "not found" });
+const app = new Hono({ strict: true });
 
 app.use(Layout);
-app.notFound(notFoundHandler);
+app.use(appendTrailingSlash());
 
 app.get("/", (c) => c.render(Home(), { title: "" }));
 app.get("/about/", (c) => c.render(About(), { title: "about" }));
-app.get("/not-found/", notFoundHandler);
+app.get("/not-found/", (c) => c.render(NotFound(), { title: "not found" }));
 
 export default app;
