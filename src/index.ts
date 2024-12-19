6d684931ee2ad1b0642e2d442bb60f3fef2cad50
Tomoki Morita <tmsongbooks215@gmail.com>
2024-12-19 17:19:32 +0900

    use chaining the methods in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,12 @@ import Home from "./routes";
 import About from "./routes/about";
 import NotFound from "./routes/not-found";
 
-const app = new Hono({ strict: true });
+const app = new Hono({ strict: true })
+  .use(Layout)
+  .use(appendTrailingSlash())
 
-app.use(Layout);
-app.use(appendTrailingSlash());
-
-app.get("/", (c) => c.render(Home(), { title: "" }));
-app.get("/about/", (c) => c.render(About(), { title: "about" }));
-app.get("/not-found/", (c) => c.render(NotFound(), { title: "not found" }));
+  .get("/", (c) => c.render(Home(), { title: "" }))
+  .get("/about/", (c) => c.render(About(), { title: "about" }))
+  .get("/not-found/", (c) => c.render(NotFound(), { title: "not found" }));
 
 export default app;
